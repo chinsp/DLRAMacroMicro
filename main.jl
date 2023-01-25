6530b94f9dd2996c28558013a6d7b87f853d77ae
Chinmay <chinmayp06@gmail.com>
2023-01-25 13:41:52 +0100

    Added some include statements for the main file

--- main.jl
+++ main.jl
@@ -5,3 +5,9 @@
 # developed by Chinmay Patwardhan, 2023
 ###
 
+include("settings.jl")
+include("solver.jl")
+
+s = Settings(); # Give the number of discretisation points for spatial domain and velocity domain as input i.e., Nx and Nv
+
+# run solver for various Settings
